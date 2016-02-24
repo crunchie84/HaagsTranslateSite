124673416197343b18cf4d19456d5f695ebd14f1
Mark van Straten <mark@q42.nl>
2016-02-24 15:46:26 +0100

    initial version of translation website

---
+++ HaagsVertaler/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using HaagsVertaler.Models;
+
+namespace HaagsVertaler.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet]
+    [Route("~/", Name = "default")]
+    public ActionResult Index()
+    {
+      return View("Home", new TranslationViewModel());
+    }
+
+    [HttpPost]
+    [Route("~/")]
+    public ActionResult IndexPost(TranslationViewModel vm)
+    {
+      if(!string.IsNullOrEmpty(vm.Source))
+        vm.Result = HaagsTranslator.Translator.Translate(vm.Source);
+
+      return View("Home", vm);
+    }
+  }
+}
---
+++ HaagsVertaler/Models/TranslationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HaagsVertaler.Models
+{
+  public class TranslationViewModel
+  {
+    [MinLength(1)]
+    [MaxLength(5000)]
+    public string Source { get; set; }
+    public string Result { get; set; }
+  }
+}
