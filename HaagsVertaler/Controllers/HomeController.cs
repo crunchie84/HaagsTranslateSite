8fb0205ef180ccf42eceb4dce732159ebc8ccff1
Roelf-Jan de Vries <roelfjan@q42.nl>
2020-04-12 21:51:47 +0200

    Dotnet - Added initial version of upgrade to Dotnet Core 3.1

--- HaagsVertaler/Controllers/HomeController.cs
+++ HaagsVertaler/Controllers/HomeController.cs
@@ -1,40 +1,39 @@
-﻿using System.Web.Mvc;
-using HaagsVertaler.Models;
+﻿using HaagsVertaler.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HaagsVertaler.Controllers
 {
   public class HomeController : Controller
   {
     [HttpGet]
-    [Route("~/", Name = "default")]
     public ActionResult Index()
     {
       return View("Home", new TranslationViewModel());
     }
 
-    [HttpPost]
-    [Route("~/")]
-    public ActionResult IndexPost(TranslationViewModel vm)
-    {
-      if(!string.IsNullOrEmpty(vm.Source))
-        vm.Result = HaagsTranslator.Translator.Translate(vm.Source);
-
-      return View("Home", vm);
-    }
-
-    [HttpGet]
-    [Route("~/api")]
-    public JsonResult TranslateApi(string text)
-    {
-      return new JsonNetResult
-      {
-        JsonRequestBehavior = JsonRequestBehavior.AllowGet,
-        Data = new TranslationViewModel
-        {
-          Source = text,
-          Result = string.IsNullOrEmpty(text) ? null : HaagsTranslator.Translator.Translate(text)
-        }
-      };
-    }
+    // [HttpPost]
+    // [Route("~/")]
+    // public ActionResult IndexPost(TranslationViewModel vm)
+    // {
+    //   if(!string.IsNullOrEmpty(vm.Source))
+    //     vm.Result = HaagsTranslator.Translator.Translate(vm.Source);
+    //
+    //   return View("Home", vm);
+    // }
+    //
+    // [HttpGet]
+    // [Route("~/api")]
+    // public JsonResult TranslateApi(string text)
+    // {
+    //   return new JsonResult
+    //   {
+    //     JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+    //     Data = new TranslationViewModel
+    //     {
+    //       Source = text,
+    //       Result = string.IsNullOrEmpty(text) ? null : HaagsTranslator.Translator.Translate(text)
+    //     }
+    //   };
+    // }
   }
 }
